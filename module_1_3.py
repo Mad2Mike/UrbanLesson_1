0ba00880a98c09c177ca73d4bece6612ecbbbe8a
Mad2Mike <mad3mike@gmail.com>
2024-08-28 09:38:31 +0300

    Lesson3 Update

--- module_1_3.py
+++ module_1_3.py
@@ -2,7 +2,7 @@ name = "Mike"
 print(f'Name: {name}')
 age = 28
 print(f'Age: {age} ')
-new_age = age + 12
-print(f'New Age: {new_age}')
+age = age + 12
+print(f'Age: {age}')
 is_student = True
 print(f'Is Student: {is_student}')
