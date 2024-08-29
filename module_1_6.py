cb9b9decffb30ce5797078ad5480b991c1e9a0c5
Mad2Mike <mad3mike@gmail.com>
2024-08-29 18:56:53 +0300

    Lesson 6 Update

--- module_1_6.py
+++ module_1_6.py
@@ -18,32 +18,3 @@ print(my_set) # Выведите на экран множество my_set (до
 my_set.update({22, 'MikeS'}) # Добавьте 2 произвольных элемента в множество my_set, которых ещё нет.
 my_set.remove('MikeS') # Удалите один любой элемент из множества my_set.
 print(my_set) # Выведите на экран измененное множество my_set.
-
-
-# Множества
-# set_ = {1, 2, 3, 4, 5, 6, 1, 2, 3, 4}
-# print(set_)
-# list_ = [1, 2, 3, 4, 1, 6, 1, 2, 3, 4]
-# print(list_)
-# list_=set(list_)
-# print(list_.discard(1))
-
-
-# Словари
-#
-# phone_book = {'Mike': [88005553535, 91812831], 'Max': 88005553535}
-# phone_book['Denis'] = 15213123123
-# phone_book['Anton'] = 1521312312123
-# del phone_book['Anton']
-#
-# phone_book.update({'Sasha': 2112312313123,
-#                    'Alex':  12312312312312})
-#
-#
-#
-# print(phone_book.keys())
-# print(phone_book.items())
-# a = phone_book.pop('Alex') #такой же метод работает для списков
-#
-# print(a)
-# print(phone_book.get('Mike', 'Такого ключа нет'))
